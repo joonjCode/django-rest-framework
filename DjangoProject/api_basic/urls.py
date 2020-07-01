ce332a74f8150fbdd26d19426a48d92697bf1b3f
Joon <jtj308@naver.com>
2020-07-02 01:17:01 +0900

    django restframework done

---
+++ DjangoProject/api_basic/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from .views import  GenericAPIView, ArticleViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('article', ArticleViewSet, basename = 'article')
+
+
+urlpatterns = [
+    path('viewset/', include(router.urls)),
+    path('viewset/<int:pk>', include(router.urls)),
+    path('generic/article/<int:id>/', GenericAPIView.as_view()),
+]
